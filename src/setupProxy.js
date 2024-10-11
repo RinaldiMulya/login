e945a0ce0e0e557bda02baa9bfc0e1afd3ba4392
IkramSkei <ikramskei@gmail.com>
2024-10-11 18:15:57 +0700

    Update setupProxy.js
    ubah path

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    '/users',  // Ubah ini ke path yang sesuai dengan endpoint API Anda
+    'users/login',  // Ubah ini ke path yang sesuai dengan endpoint API Anda
     createProxyMiddleware({
       target: 'https://inventaris-app-backend.vercel.app', 
       changeOrigin: true,
